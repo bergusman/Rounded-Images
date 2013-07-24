bbf3637932308701355155916696ac8f169cbe71
Vitaliy Berg <bergusman@gmail.com>
2013-07-24 16:38:43 +0400

    Initial Commit

---
+++ Rounded Images/VBAppDelegate.h
@@ -0,0 +1,19 @@
+//
+//  VBAppDelegate.h
+//  Rounded Images
+//
+//  Created by Vitaly Berg on 24.07.13.
+//  Copyright (c) 2013 Vitaly Berg. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@class VBViewController;
+
+@interface VBAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@property (strong, nonatomic) VBViewController *viewController;
+
+@end
---
+++ Rounded Images/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  Rounded Images
+//
+//  Created by Vitaly Berg on 24.07.13.
+//  Copyright (c) 2013 Vitaly Berg. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "VBAppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([VBAppDelegate class]));
+    }
+}
